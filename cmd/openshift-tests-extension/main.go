2ba2b883db82e46f78da9be642d017f3e94c9c2a
Radomir Vagner <rvagner@redhat.com>
2026-07-30 16:36:37 +0200

    UPSTREAM: <carry>: Add OTE extensions for upstream e2e tests

---
+++ cmd/openshift-tests-extension/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	et "github.com/openshift-eng/openshift-tests-extension/pkg/extension/extensiontests"
+	"github.com/spf13/cobra"
+
+	"github.com/openshift-eng/openshift-tests-extension/pkg/cmd"
+	e "github.com/openshift-eng/openshift-tests-extension/pkg/extension"
+	g "github.com/openshift-eng/openshift-tests-extension/pkg/ginkgo"
+
+	"k8s.io/kubernetes/test/e2e/framework"
+	"k8s.io/kubernetes/test/e2e/framework/config"
+
+	_ "sigs.k8s.io/azuredisk-csi-driver/test/e2e"
+)
+
+func main() {
+	os.Setenv("SKIP_DRIVER_INSTALL", "true")
+
+	if os.Getenv("KUBECONFIG") == "" {
+		os.Setenv("KUBECONFIG", filepath.Join(os.Getenv("HOME"), ".kube", "config"))
+	}
+	config.CopyFlags(config.Flags, flag.CommandLine)
+	framework.RegisterCommonFlags(flag.CommandLine)
+	framework.RegisterClusterFlags(flag.CommandLine)
+	flag.Parse()
+	framework.AfterReadingAllFlags(&framework.TestContext)
+
+	registry := e.NewRegistry()
+	ext := e.NewExtension("openshift", "payload", "azure-disk-csi-driver-test")
+
+	ext.AddSuite(e.Suite{
+		Name:    "openshift/csi/azure-disk",
+		Parents: []string{"openshift/conformance/parallel"},
+	})
+
+	specs, err := g.BuildExtensionTestSpecsFromOpenShiftGinkgoSuite()
+	if err != nil {
+		panic(fmt.Sprintf("failed to build extension test specs from ginkgo: %s", err.Error()))
+	}
+
+	// Only run on Azure platform
+	specs.Include(et.PlatformEquals("azure"))
+
+	// Exclude pre-provisioned tests (require in-process CSI driver instance)
+	specs.Select(et.NameContains("Pre-Provisioned")).Exclude("true")
+
+	// Exclude retain reclaim policy test (uses in-process CSI driver for cleanup)
+	specs.Select(et.NameContains(`reclaimPolicy "Retain"`)).Exclude("true")
+
+	// Exclude cross-region snapshot tests (require location from BeforeSuite)
+	specs.Select(et.NameContains("snapshot cross region")).Exclude("true")
+
+	ext.AddSpecs(specs)
+	registry.Register(ext)
+
+	root := &cobra.Command{
+		Long: "Azure Disk CSI Driver E2E Tests for OpenShift",
+	}
+
+	root.AddCommand(cmd.DefaultExtensionCommands(registry)...)
+
+	if err := root.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -22,10 +22,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/onsi/ginkgo/v2"
@@ -41,52 +39,17 @@ import (
 	"sigs.k8s.io/azuredisk-csi-driver/test/utils/credentials"
 )
 
-const (
-	kubeconfigEnvVar       = "KUBECONFIG"
-	reportDirEnvVar        = "ARTIFACTS"
-	testMigrationEnvVar    = "TEST_MIGRATION"
-	testWindowsEnvVar      = "TEST_WINDOWS"
-	testWinServerVerEnvVar = "WINDOWS_SERVER_VERSION"
-	cloudNameEnvVar        = "AZURE_CLOUD_NAME"
-	defaultReportDir       = "/workspace/_artifacts"
-	inTreeStorageClass     = "kubernetes.io/azure-disk"
-)
-
-var (
-	azurediskDriver           azuredisk.CSIDriver
-	isUsingInTreeVolumePlugin = os.Getenv(driver.AzureDriverNameVar) == inTreeStorageClass
-	isTestingMigration        = os.Getenv(testMigrationEnvVar) != ""
-	isWindowsCluster          = os.Getenv(testWindowsEnvVar) != ""
-	winServerVer              = os.Getenv(testWinServerVerEnvVar)
-	isAzureStackCloud         = strings.EqualFold(os.Getenv(cloudNameEnvVar), "AZURESTACKCLOUD")
-	isWindowsHPCDeployment    = strings.EqualFold(os.Getenv("WINDOWS_USE_HOST_PROCESS_CONTAINERS"), "true")
-	isCapzTest                = os.Getenv("NODE_MACHINE_TYPE") != ""
-	location                  string
-	supportsZRS               bool
-)
-
-type testCmd struct {
-	command  string
-	args     []string
-	startLog string
-	endLog   string
-}
-
 var _ = ginkgo.BeforeSuite(func(ctx ginkgo.SpecContext) {
 	log.Println(driver.AzureDriverNameVar, os.Getenv(driver.AzureDriverNameVar), fmt.Sprintf("%v", isUsingInTreeVolumePlugin))
 	log.Println(testMigrationEnvVar, os.Getenv(testMigrationEnvVar), fmt.Sprintf("%v", isTestingMigration))
 	log.Println(testWindowsEnvVar, os.Getenv(testWindowsEnvVar), fmt.Sprintf("%v", isWindowsCluster))
 	log.Println(testWinServerVerEnvVar, os.Getenv(testWinServerVerEnvVar), fmt.Sprintf("%v", winServerVer))
 
-	// k8s.io/kubernetes/test/e2e/framework requires env KUBECONFIG to be set
-	// it does not fall back to defaults
 	if os.Getenv(kubeconfigEnvVar) == "" {
 		kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
 		os.Setenv(kubeconfigEnvVar, kubeconfig)
 	}
 
-	// Default storage driver configuration is CSI. Freshly built
-	// CSI driver is installed for that case.
 	if isTestingMigration || !isUsingInTreeVolumePlugin {
 		creds, err := credentials.CreateAzureCredentialFile()
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
@@ -97,30 +60,12 @@ var _ = ginkgo.BeforeSuite(func(ctx ginkgo.SpecContext) {
 
 		location = creds.Location
 		supportZRSRegions := []string{
-			"southafricanorth",
-			"eastasia",
-			"southeastasia",
-			"australiaeast",
-			"brazilsouth",
-			"westeurope",
-			"northeurope",
-			"francecentral",
-			"centralindia",
-			"italynorth",
-			"japaneast",
-			"koreacentral",
-			"norwayeast",
-			"polandcentral",
-			"qatarcentral",
-			"swedencentral",
-			"switzerlandnorth",
-			"uaenorth",
-			"uksouth",
-			"eastus",
-			"eastus2",
-			"southcentralus",
-			"westus2",
-			"westus3",
+			"southafricanorth", "eastasia", "southeastasia", "australiaeast",
+			"brazilsouth", "westeurope", "northeurope", "francecentral",
+			"centralindia", "italynorth", "japaneast", "koreacentral",
+			"norwayeast", "polandcentral", "qatarcentral", "swedencentral",
+			"switzerlandnorth", "uaenorth", "uksouth",
+			"eastus", "eastus2", "southcentralus", "westus2", "westus3",
 		}
 		supportsZRS = false
 		for _, region := range supportZRSRegions {
@@ -131,14 +76,12 @@ var _ = ginkgo.BeforeSuite(func(ctx ginkgo.SpecContext) {
 		}
 
 		if os.Getenv("SKIP_DRIVER_INSTALL") != "true" {
-			// Install Azure Disk CSI Driver on cluster from project root
 			e2eBootstrap := testCmd{
 				command:  "make",
 				args:     []string{"e2e-bootstrap"},
 				startLog: "Installing Azure Disk CSI Driver...",
 				endLog:   "Azure Disk CSI Driver installed",
 			}
-
 			createMetricsSVC := testCmd{
 				command:  "make",
 				args:     []string{"create-metrics-svc"},
@@ -167,8 +110,6 @@ var _ = ginkgo.BeforeSuite(func(ctx ginkgo.SpecContext) {
 })
 
 var _ = ginkgo.AfterSuite(func(_ ginkgo.SpecContext) {
-	// Default storage driver configuration is CSI. Freshly built
-	// CSI driver is installed for that case.
 	if isTestingMigration || isUsingInTreeVolumePlugin {
 		cmLog := testCmd{
 			command:  "bash",
@@ -223,7 +164,6 @@ var _ = ginkgo.AfterSuite(func(_ ginkgo.SpecContext) {
 				startLog: "delete metrics service...",
 				endLog:   "metrics service deleted",
 			}
-
 			e2eTeardown := testCmd{
 				command:  "make",
 				args:     []string{"e2e-teardown"},
@@ -233,7 +173,6 @@ var _ = ginkgo.AfterSuite(func(_ ginkgo.SpecContext) {
 			execTestCmd([]testCmd{deleteMetricsSVC, e2eTeardown})
 
 			if !isTestingMigration {
-				// install Azure Disk CSI Driver deployment scripts test
 				installDriver := testCmd{
 					command:  "bash",
 					args:     []string{"deploy/install-driver.sh", "master", "windows,snapshot,local"},
@@ -242,7 +181,6 @@ var _ = ginkgo.AfterSuite(func(_ ginkgo.SpecContext) {
 				}
 				execTestCmd([]testCmd{installDriver})
 
-				// run example deployment again
 				createExampleDeployment := testCmd{
 					command:  "bash",
 					args:     []string{"hack/verify-examples.sh", testOS, cloud},
@@ -251,7 +189,6 @@ var _ = ginkgo.AfterSuite(func(_ ginkgo.SpecContext) {
 				}
 				execTestCmd([]testCmd{createExampleDeployment})
 
-				// uninstall Azure Disk CSI Driver deployment scripts test
 				uninstallDriver := testCmd{
 					command:  "bash",
 					args:     []string{"deploy/uninstall-driver.sh", "master", "windows,snapshot,local"},
@@ -276,86 +213,6 @@ func TestE2E(t *testing.T) {
 	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, "AzureDisk CSI Driver End-to-End Tests", r)
 }
 
-func execTestCmd(cmds []testCmd) {
-	err := os.Chdir("../..")
-	gomega.Expect(err).NotTo(gomega.HaveOccurred())
-	defer func() {
-		err := os.Chdir("test/e2e")
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-	}()
-
-	projectRoot, err := os.Getwd()
-	gomega.Expect(err).NotTo(gomega.HaveOccurred())
-	gomega.Expect(strings.HasSuffix(projectRoot, "azuredisk-csi-driver")).To(gomega.Equal(true))
-
-	for _, cmd := range cmds {
-		log.Println(cmd.startLog)
-		cmdSh := exec.Command(cmd.command, cmd.args...)
-		cmdSh.Dir = projectRoot
-		cmdSh.Stdout = os.Stdout
-		cmdSh.Stderr = os.Stderr
-		err = cmdSh.Run()
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		log.Println(cmd.endLog)
-	}
-}
-
-func skipIfTestingInWindowsCluster() {
-	if isWindowsCluster {
-		ginkgo.Skip("test case not supported by Windows clusters")
-	}
-}
-
-func skipIfUsingInTreeVolumePlugin() {
-	if isUsingInTreeVolumePlugin {
-		ginkgo.Skip("test case is only available for CSI drivers")
-	}
-}
-
-func skipIfOnAzureStackCloud() {
-	if isAzureStackCloud {
-		ginkgo.Skip("test case not supported on Azure Stack Cloud")
-	}
-}
-
-func skipIfNotZRSSupported() {
-	if !supportsZRS {
-		ginkgo.Skip("test case not supported on regions without ZRS")
-	}
-}
-
-func convertToPowershellorCmdCommandIfNecessary(command string) string {
-	if !isWindowsCluster {
-		return command
-	}
-
-	switch command {
-	case "echo 'hello world' > /mnt/test-1/data && grep 'hello world' /mnt/test-1/data":
-		return "echo 'hello world' | Out-File -FilePath C:\\mnt\\test-1\\data.txt; Get-Content C:\\mnt\\test-1\\data.txt | findstr 'hello world'"
-	case "touch /mnt/test-1/data":
-		return "echo $null >> C:\\mnt\\test-1\\data"
-	case "while true; do echo $(date -u) >> /mnt/test-1/data; sleep 3600; done":
-		return "while (1) { Add-Content -Encoding Ascii C:\\mnt\\test-1\\data.txt $(Get-Date -Format u); sleep 3600 }"
-	case "echo 'hello world' >> /mnt/test-1/data && while true; do sleep 3600; done":
-		return "echo 'hello world' | Out-File -Append -FilePath C:\\mnt\\test-1\\data.txt; Get-Content C:\\mnt\\test-1\\data.txt | findstr 'hello world'; Start-Sleep 3600"
-	case "echo 'hello world' > /mnt/test-1/data && echo 'hello world' > /mnt/test-2/data && echo 'hello world' > /mnt/test-3/data && grep 'hello world' /mnt/test-1/data && grep 'hello world' /mnt/test-2/data && grep 'hello world' /mnt/test-3/data":
-		return "echo 'hello world' | Out-File -FilePath C:\\mnt\\test-1\\data.txt; Get-Content C:\\mnt\\test-1\\data.txt | findstr 'hello world'; echo 'hello world' | Out-File -FilePath C:\\mnt\\test-2\\data.txt; Get-Content C:\\mnt\\test-2\\data.txt | findstr 'hello world'; echo 'hello world' | Out-File -FilePath C:\\mnt\\test-3\\data.txt; Get-Content C:\\mnt\\test-3\\data.txt | findstr 'hello world'"
-	case "while true; do sleep 5; done":
-		return "while ($true) { Start-Sleep 5 }"
-	case "echo 'hello world' > /mnt/test-1/data":
-		return "echo 'hello world' | Out-File -FilePath C:\\mnt\\test-1\\data.txt"
-	case "echo 'overwrite' > /mnt/test-1/data; sleep 3600":
-		return "echo 'overwrite' | Out-File -FilePath C:\\mnt\\test-1\\data.txt; Start-Sleep 3600"
-	case "grep 'hello world' /mnt/test-1/data":
-		return "Get-Content C:\\mnt\\test-1\\data.txt | findstr 'hello world'"
-	case "df -h | grep /mnt/test- | awk '{print $2}' | grep -E '19|20'":
-		return "fsutil volume diskfree C:\\mnt\\ | Select-String 'Total bytes' | Select-String -Pattern '19|20'"
-	}
-
-	return command
-}
-
-// handleFlags sets up all flags and parses the command line.
 func TestMain(m *testing.M) {
 	config.CopyFlags(config.Flags, flag.CommandLine)
 	framework.RegisterCommonFlags(flag.CommandLine)
@@ -363,11 +220,4 @@ func TestMain(m *testing.M) {
 	framework.AfterReadingAllFlags(&framework.TestContext)
 	flag.Parse()
 	os.Exit(m.Run())
-}
-
-func getFSType(IsWindowsCluster bool) string {
-	if IsWindowsCluster {
-		return "ntfs"
-	}
-	return "ext4"
 }
